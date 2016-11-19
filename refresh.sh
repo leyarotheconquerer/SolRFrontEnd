0d8fcafa80e8c68e65504f3fbfbd731b6fcd1881
israelterrill <israelterrill@letu.edu>
2016-11-19 15:16:22 -0600

    Add Linux refresh script.

---
+++ refresh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./node_modules/.bin/browserify index.js \
+        --require react \
+        --require react-dom \
+        --transform [ babelify --presets [ react es2015 stage-2 ] ] \
+        --standalone FacetedSearch \
+        -o web.js
+
